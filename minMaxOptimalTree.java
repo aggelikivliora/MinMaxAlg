adf1c85ce6335417cab634aa4c04d3a631f576c7
aggeliki vliora <57294380+aggelikivliora@users.noreply.github.com>
2023-11-16 16:57:34 +0200

    Update minMaxOptimalTree.java

--- minMaxOptimalTree.java
+++ minMaxOptimalTree.java
@@ -5,134 +5,84 @@ import java.io.IOException;
 import java.util.ArrayList;
 import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Integer.MIN_VALUE;
+import org.json.JSONException;
 public class minMaxOptimalTree extends minMaxTree{
-    //kataskeyasths : ftiaxnw dentro
-    public minMaxOptimalTree(String jstr){
+    public minMaxOptimalTree(String jstr) throws JSONException,IllegalArgumentException{
         super(jstr);
-    }// 86 PRUNED
-    //epilyetai o algori8mos minmax me alpha beta pruning
+    }
+    
+    @Override
     public double minMax(){
-        //System.out.println("AAAAAAAAAAAAAAAAA");
-        minMaxOptimalRecursive(getRoot());
+        minMaxOptimalRecursive(getRoot(), 0);
         return getRoot().getValue();
     }
     public double simpleMinMax(){
         return super.minMax();
     }
-    public void minMaxOptimalRecursive(Object nd){
+    public void minMaxOptimalRecursive(Object nd, int v){
         if(nd instanceof maximizer){
-            maximizer curr = new maximizer();
+            maximizer curr ;
             curr = (maximizer)nd;
             for(int i=0; i<curr.getChildrenSize(); i++){
                 curr.getChild(i).alpha=curr.alpha;
                 curr.getChild(i).beta=curr.beta;
-                minMaxOptimalRecursive(curr.getChild(i));
+                minMaxOptimalRecursive(curr.getChild(i), v+1);
                 if(curr.getChild(i).getValue()>curr.alpha)
                 {
                     curr.alpha=curr.getChild(i).getValue();
-                    //System.out.println("(("+i+"))MAX:");
-                    //System.out.println("now alpha:<<"+curr.alpha+">>");
-                    //System.out.println("now beta=<<"+curr.beta+">>");
                     if(curr.beta<=curr.alpha){
                         curr.setValue(curr.alpha);
-                        //System.out.println(curr.alpha);
                         return;
                     }
                 }
             }
             double value=curr.maxNode();
             curr.setValue(value);
-            //System.out.println("max(("+value+"))");
             return;
         }
         if(nd instanceof minimizer){
-            minimizer curr = new minimizer();
+            minimizer curr ;
             curr = (minimizer)nd;
             for(int i=0; i<curr.getChildrenSize(); i++){
                 curr.getChild(i).beta=curr.beta;
                 curr.getChild(i).alpha=curr.alpha;
-                minMaxOptimalRecursive(curr.getChild(i));
+                minMaxOptimalRecursive(curr.getChild(i), v+1);
                 if(curr.getChild(i).getValue()<curr.beta)
                 {
                     curr.beta=curr.getChild(i).getValue();
-                    //System.out.println(i + ") MINIMIZER:");
-                    //System.out.println("now alpha=<<" + curr.alpha + ">>");
-                    //System.out.println("now beta=<<" + curr.beta + ">>");
                     if(curr.beta<=curr.alpha){
                         curr.setValue(curr.beta);
-                        //System.out.print("exit:"+curr.beta);
-                        /*if(curr!=getRoot()){
-                            System.out.println(", parent_alpha:(("+curr.getParent().alpha+"))");
-                        }*/
                         return;
                     }
                 }
             }
             double value=curr.minNode();
             curr.setValue(value);
-            //System.out.print("min(("+value+"))");
+            //System.out.println("["+value+","+curr.alpha+","+curr.beta+"]\n"); 
+            
             return;
         }
         if(nd instanceof InternalNode){
-            InternalNode curr = (InternalNode)nd;
+            InternalNode curr; 
+            curr = (InternalNode)nd;
+            //System.out.println(curr.getValue()); 
             curr.alpha = MAX_VALUE;
             curr.beta = MIN_VALUE;
         }
-    }/*
-    public double minMaxOptimalRecursive(Object nd){
-        if(nd instanceof InternalNode){
-            InternalNode curr = (InternalNode)nd;
-            curr.alpha = MAX_VALUE;
-            curr.beta = MIN_VALUE;
-            return curr.getValue();
-        }
-        if(nd instanceof maximizer){
-            maximizer curr = new maximizer();
-            curr = (maximizer)nd;
-            for(int i=0; i<curr.getChildrenSize(); i++){
-            
-            }
-        }
-    }*/
+    }
+    
     // epistrefetai to optimal path
     @Override
     public ArrayList<Integer> optimalPath(){
         ArrayList<Integer> path = new ArrayList<>();
         path = findPath(getRoot(), path);
         return path;
-        /*while(true){
-            if(nd instanceof maximizer){
-                //System.out.println(";-) ");
-                maximizer curr = new maximizer();
-                curr = (maximizer)nd;
-                for(int i=0; i<curr.getChildrenSize(); i++){
-                    if(curr.getChild(i).getValue()==getRoot().getValue()){
-                        path.add(i);
-                        nd = curr.getChild(i);
-                        break;
-                    }
-                }
-            }
-            else if(nd instanceof minimizer){
-                minimizer curr = new minimizer();
-                curr = (minimizer)nd;
-                for(int i=0; i<curr.getChildrenSize(); i++){
-                    if(curr.getChild(i).getValue()==getRoot().getValue()){
-                        path.add(i);
-                        nd = curr.getChild(i);
-                        break;
-                    }
-                }
-            }
-            else{
-               return path;
-            }
-        }*/
     }
     public ArrayList<Integer> findPath(Object nd, ArrayList<Integer> path){
         ArrayList<Integer> temp = new ArrayList<>();
         if(nd instanceof maximizer){
-            maximizer curr = (maximizer)nd;
+            maximizer curr;
+            curr = (maximizer)nd;
             for(int i=0; i<curr.getChildrenSize(); i++){
                 if(curr.getChild(i).getValue()==getRoot().getValue()){
                     path.add(i);
@@ -145,7 +95,8 @@ public class minMaxOptimalTree extends minMaxTree{
             return temp;
         }
         if(nd instanceof minimizer){
-            minimizer curr = (minimizer)nd;
+            minimizer curr;
+            curr = (minimizer)nd;
             for(int i=0; i<curr.getChildrenSize(); i++){
                 if(curr.getChild(i).getValue()==getRoot().getValue()){
                     path.add(i);
@@ -166,19 +117,19 @@ public class minMaxOptimalTree extends minMaxTree{
     } 
     public double countPrunedNodes(Object nd,double pnodes){
         if(nd instanceof maximizer){
-            maximizer curr =(maximizer)nd;
+            maximizer curr;
+            curr =(maximizer)nd;
             for(int i=0; i<curr.getChildrenSize(); i++){
                 pnodes=countPrunedNodes(curr.getChild(i),pnodes);
-            //System.out.println(" <MAX> "+curr.getValue()+" :: ("+curr.alpha+","+curr.beta+")");                
             }
             if((curr.beta==MAX_VALUE)&&(curr.alpha==MIN_VALUE)){
-               // System.out.print("PRUNED!!! =");
                 pnodes++;
             }
             return pnodes;
         }
         else if(nd instanceof minimizer){
-            minimizer curr = (minimizer)nd;
+            minimizer curr;
+            curr = (minimizer)nd;
             for(int i=0; i<curr.getChildrenSize(); i++){
                 pnodes=countPrunedNodes(curr.getChild(i), pnodes);          
             //System.out.println(" <MINIMIZED> "+curr.getValue()+" :: ("+curr.alpha+","+curr.beta+")");                
@@ -190,18 +141,17 @@ public class minMaxOptimalTree extends minMaxTree{
             return pnodes;
         } 
         else if(nd instanceof InternalNode){
-            InternalNode curr  = (InternalNode)nd;
+            InternalNode curr;
+            curr = (InternalNode)nd;
             if((curr.alpha==MIN_VALUE)&&(curr.beta==MAX_VALUE)){
                 //System.out.print("PRUNED!!! =");
                 pnodes++;
             }
             //System.out.println(" <leaf> "+curr.getValue()+" :: ("+curr.alpha+","+curr.beta+")");
-
         }
         return pnodes;
     }
     
-
     @Override
     public String toString(){
         int flag=0;
@@ -214,61 +164,92 @@ public class minMaxOptimalTree extends minMaxTree{
             return s;
         }
         StringBuilder sb = new StringBuilder();
-        sb = OpTreeToStringB(getRoot(),sb);
+        sb = OpTreeToStringB(getRoot(),sb,2);
         return ""+sb;
     }
-    public StringBuilder OpTreeToStringB(Object nd, StringBuilder sb){
+    public StringBuilder OpTreeToStringB(Object nd, StringBuilder sb,int vathos){
        if(nd instanceof maximizer){
-            maximizer curr = new maximizer();
+            maximizer curr;
             curr = (maximizer)nd;
-            sb.append("{\n"+'"'+"type"+'"'+":"+'"'+"max"+'"'+","+"\n");
-            sb.append('"'+"value"+'"'+"=");
-            sb.append(curr.getValue());
-            sb.append(",\n");
+            sb.append(spaces(vathos-2));
+            sb.append("{\n");
+            sb.append(spaces(vathos));
+            sb.append('"'+"type"+'"'+": "+'"'+"max"+'"'+","+"\n");
             //pruned
             if((curr.beta==MAX_VALUE)&&(curr.alpha==MIN_VALUE)){
-                sb.append(","+'"'+"pruned"+'"'+"="+"true"+",\n");
+                sb.append(spaces(vathos));
+                sb.append('"'+"pruned"+'"'+": "+"true"+",\n");
             }
-            sb.append('"'+"children"+'"'+":[\n");
+            else{
+                sb.append(spaces(vathos));
+                sb.append('"'+"value"+'"'+": ");
+                sb.append(String.format("%.2f",curr.getValue()));
+                sb.append(",\n");
+            }
+            sb.append(spaces(vathos));
+            sb.append('"'+"children"+'"'+": [\n");
             for(int i=0; i<curr.getChildrenSize(); i++){
-                sb = OpTreeToStringB(curr.getChild(i), sb);
+                sb = OpTreeToStringB(curr.getChild(i), sb, vathos+2);
                 if(i!=curr.getChildrenSize()-1){
-                    sb.append(",");
+                    sb.append(",\n");
                 }
             }    
-            sb.append("]\n}\n");
+            sb.append("\n");
+            sb.append(spaces(vathos));
+            sb.append("]\n");
+            sb.append(spaces(vathos-2));
+            sb.append("}");
         }
         else if(nd instanceof minimizer){
-            minimizer curr = new minimizer();
+            minimizer curr;
             curr = (minimizer)nd;
-            sb.append("{\n"+'"'+"type"+'"'+":"+'"'+"min"+'"'+",\n");
-            sb.append('"'+"value"+'"'+"=");
-            sb.append(curr.getValue());
-            sb.append(",");
+            sb.append(spaces(vathos-2));
+            sb.append("{\n");
+            sb.append(spaces(vathos));
+            sb.append('"'+"type"+'"'+": "+'"'+"min"+'"'+",\n");
             //pruned
             if((curr.beta==MAX_VALUE)&&(curr.alpha==MIN_VALUE)){
-                sb.append(","+'"'+"pruned"+'"'+"="+"true"+",\n");
+                sb.append(spaces(vathos));
+                sb.append('"'+"pruned"+'"'+": "+"true"+",\n");
             }
-            sb.append('"'+"children"+'"'+":[");
+            else{
+                sb.append(spaces(vathos));
+                sb.append('"'+"value"+'"'+": ");
+                sb.append(String.format("%.2f",curr.getValue()));
+                sb.append(",\n");
+            }
+            sb.append(spaces(vathos));
+            sb.append('"'+"children"+'"'+": [\n");
             for(int i=0; i<curr.getChildrenSize(); i++){
-                sb = OpTreeToStringB(curr.getChild(i),sb);
+                sb = OpTreeToStringB(curr.getChild(i),sb, vathos+2);
                 if(i!=curr.getChildrenSize()-1){
-                    sb.append(",");
+                    sb.append(",\n");
                 }
             }
-            sb.append("]}");
+            sb.append("\n");
+            sb.append(spaces(vathos));
+            sb.append("]\n");
+            sb.append(spaces(vathos-2));
+            sb.append("}");
         }
         else{
-            InternalNode curr = new InternalNode();
+            InternalNode curr;
             curr = (InternalNode)nd;
             double num = curr.getValue();
-            sb.append("{"+'"'+"type"+'"'+":"+'"'+"leaf"+'"'+","+'"'+"value"+":");
-            sb.append(num);
-            //pruned
-            //System.out.println(curr.alpha+","+curr.beta);
+            sb.append(spaces(vathos-2));
+            sb.append("{\n");
+            sb.append(spaces(vathos));
+            sb.append('"'+"type"+'"'+": "+'"'+"leaf"+'"'+",\n");
+            sb.append(spaces(vathos));
+            sb.append('"'+"value"+'"'+": ");
+            sb.append(String.format("%.2f",num));
             if((curr.alpha==MIN_VALUE)&&(curr.beta==MAX_VALUE)){
-                sb.append(","+'"'+"pruned"+'"'+"="+"true");
+                sb.append(",\n");
+                sb.append(spaces(vathos));
+                sb.append('"'+"pruned"+'"'+": "+"true");
             }
+            sb.append("\n");
+            sb.append(spaces(vathos-2));
             sb.append("}");
             return sb;
         } 
@@ -276,25 +257,20 @@ public class minMaxOptimalTree extends minMaxTree{
     }
     
 
+    @Override
     public void toFile(File file){
         try {
             file.createNewFile();
-            FileWriter myWriter = new FileWriter(file);
-            /*if((getRoot().alpha==MIN_VALUE)&&(getRoot().beta==MAX_VALUE)){
-                //System.out.println(getRoot().alpha+","+getRoot().beta);
-                String s = super.toString();
+            try (FileWriter myWriter = new FileWriter(file)) {
+                String s = toString();
                 myWriter.write(s);
-                myWriter.close();
-                return;
-            }*/
-            //System.out.println(getRoot().alpha+","+getRoot().beta);
-            String s = toString();
-            myWriter.write(s);
-            myWriter.close();
+            }
+            System.out.print("\nOK\n\n");
         } catch (IOException e) {
-            System.out.println("\n\nUnable to write '"+file+"'\n");
+            System.out.print("\nUnable to write '"+file+"'\n\n");
             //e.printStackTrace();
         }
         
     }
 }
+
