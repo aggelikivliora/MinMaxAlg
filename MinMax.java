ce69e911f4aaa9c3808c6313bea05f21833b4df7
aggeliki vliora <57294380+aggelikivliora@users.noreply.github.com>
2023-11-16 16:55:04 +0200

    Update MinMax.java

--- MinMax.java
+++ MinMax.java
@@ -5,26 +5,22 @@ import org.json.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class MinMax {
     String Jstr;
     minMaxOptimalTree tree;
-    //minMaxTree tree;
     public MinMax(String Jstr){
         this.Jstr = Jstr; 
         this.tree = null;
-        //this.opTree=null;
     }
     public MinMax(){
         Jstr=null;
         tree=null;
-        //opTree=null;
     }
     public void setStr(String str){
         this.Jstr = str;
     }
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         MinMax mainMinMax = new MinMax();
         Scanner sc = new Scanner(System.in);
@@ -37,69 +33,17 @@ public class MinMax {
             System.out.print(String.format("%-15s %s","-c",":  calculate tree using min-max algorithm\n"));
             System.out.print(String.format("%-15s %s","-p",":  calculate tree using min-max and alpha-beta pruning optimization\n"));
             System.out.print(String.format("%-15s %s","-q",":  quit this program"));
-            System.out.print("\n\n$> ");
-            try/*(Scanner sc = new Scanner(System.in))*/{
-            //Scanner sc = new Scanner(System.in);
-            String strr = sc.nextLine();
-            //sc.close();
-            //sc.next();
-            //System.out.println(strr.length());
-            if(strr.length()<2){
-                //sc.next();
-                //sc.close();
-                continue;
-            }
-
-            String str = strr.replaceAll("\\s","");
-            String ch = str.substring(0,2);
-            int start=2, end=str.length();
+            System.out.print("\n\n$>  ");
+            try{
+                String strr = sc.nextLine();
+                if(strr.length()<2){
+                    continue;
+                }
+                String str = strr.replaceAll("\\s","");
+                String ch = str.substring(0,2);
+                int start=2, end=str.length();
             
-            if("-i".equals(ch)){
-                for(int i=0; i<str.length(); i++){
-                    if(str.charAt(i)=='<'){
-                        start = i;
-                        break;
-                    }
-                }
-                String fileName = str.substring(start,end);
-                File file = new File(fileName);
-                if(!file.exists()){
-                    System.out.println("\nUnable to find '"+fileName+"'\n\n");
-                    continue;
-                }
-                if(!file.canRead()){
-                    System.out.println("\nUnable to open '"+fileName+"'\n\n");
-                    continue;
-                }
-                StringBuilder strBuilder = new StringBuilder();
-                try(Scanner sc2 = new Scanner(new File(fileName))) {
-                    while( sc2.hasNextLine() ) {
-                    String string = sc2.nextLine();
-                    strBuilder.append(string);
-                    strBuilder.append("\n");
-                    }
-                } catch(IOException ex) {
-                    //ex.printStackTrace();
-                    continue;
-                } catch(IllegalArgumentException ex) {
-                    System.out.println("\nInvalid format\n\n");
-                    //ex.printStackTrace();
-                    continue;
-                    //ex.printStackTrace();
-                }
-                // // // //CHECK IF THE STRING IS JSON  
-                String jsonString = strBuilder.toString();
-                MinMax myMinMax = new MinMax(jsonString);
-                if(!(myMinMax.isJson(jsonString))){
-                    continue;
-                }
-                mainMinMax = myMinMax;
-                mainMinMax.tree = new minMaxOptimalTree(mainMinMax.Jstr);
-                System.out.println("\nOK\n\n");
-            }//#################################################################
-            if("-j".equals(ch))
-            {   ////if we re having a file
-                if(str.length()>2){
+                if("-i".equals(ch)){
                     for(int i=0; i<str.length(); i++){
                         if(str.charAt(i)=='<'){
                             start = i;
@@ -108,81 +52,132 @@ public class MinMax {
                     }
                     String fileName = str.substring(start,end);
                     File file = new File(fileName);
-                    if(file.exists()){
-                        System.out.println("\nFile '"+fileName+"' already exists\n\n");
+                    if(!file.exists()){
+                        System.out.print("\nUnable to find '"+fileName+"'\n\n");
                         continue;
                     }
-                    if(mainMinMax.tree==null){
-                        if(mainMinMax.Jstr!=null){
-                            System.out.println("\n"+mainMinMax.Jstr+"\n\n");
+                    if(!file.canRead()){
+                        System.out.print("\nUnable to open '"+fileName+"'\n\n");
+                        continue;
+                    }
+                    StringBuilder strBuilder = new StringBuilder();
+                    try(Scanner sc2 = new Scanner(new File(fileName))) {
+                        while( sc2.hasNextLine() ) {
+                        String string = sc2.nextLine();
+                        strBuilder.append(string);
+                        strBuilder.append("\n");
                         }
+                    } catch(IOException ex) {
+                        continue;
+                    } catch(IllegalArgumentException ex) {
+                        System.out.print("\nInvalid format\n\n");
                         continue;
                     }
-                    mainMinMax.tree.toFile(file);
-                    System.out.println("\nOK\n\n");
-                }////if we re not having a file
-                else{
-                    System.out.println("\n"+mainMinMax.tree.toString()+"\n\n");
+                    // // // //CHECK IF THE STRING IS JSON  
+                    String jsonString = strBuilder.toString();
+                    mainMinMax = new MinMax(jsonString);
+                    try {
+                        mainMinMax.tree = new minMaxOptimalTree(mainMinMax.Jstr);
+                    } catch (JSONException | IllegalArgumentException ex) {
+                        System.out.print("\nInvalid format\n\n");
+                        continue;
+                    }
+                    System.out.print("\nOK\n\n");
+                }//#################################################################
+                else if("-j".equals(ch))
+                {   ////if we re having a file
+                    if(mainMinMax.tree==null){
+                        System.out.print("\nNot OK\n\n");
+                        continue;
+                    }
+                    if(str.length()>2){
+                        for(int i=0; i<str.length(); i++){
+                            if(str.charAt(i)=='<'){
+                                start = i;
+                                break;
+                            }
+                        }
+                        String fileName = str.substring(start,end);
+                        File file = new File(fileName);
+                        if(file.exists()){
+                            System.out.print("\nFile '"+fileName+"' already exists\n\n");
+                            continue;
+                        }
+                        if(mainMinMax.tree==null){
+                            if(mainMinMax.Jstr!=null){
+                                System.out.print("\n"+mainMinMax.Jstr+"\n\n");
+                            }
+                            continue;
+                        }
+                        mainMinMax.tree.toFile(file);
+                        //System.out.println("\nOK\n\n");
+                    }////if we re not having a file
+                    else{
+                        System.out.print("\n"+mainMinMax.tree.toString()+"\n\n");
+                    }
                 }
-            }
-            if("-d".equals(ch))
-            {
+                else if("-d".equals(ch))
+                {
            
-            }
-            if("-c".equals(ch))
-            {// SIMPLE MINMAX
-                if(mainMinMax.tree!=null){
-                    double rt=mainMinMax.tree.simpleMinMax();
-                    ArrayList<Integer> path = mainMinMax.tree.optimalPath();
-                    System.out.print("\n");
-                    for(int i=0; i<path.size(); i++){
-                        System.out.print(path.get(i));
-                        if(i!=path.size()-1){System.out.print(", ");}
+                }
+                else if("-c".equals(ch))
+                {// SIMPLE MINMAX
+                    if((mainMinMax.tree!=null) && (mainMinMax.Jstr!=null) ){
+                        mainMinMax.tree.simpleMinMax();
+                        ArrayList<Integer> path = mainMinMax.tree.optimalPath();
+                        System.out.print("\n");
+                        for(int i=0; i<path.size(); i++){
+                            System.out.print(path.get(i));
+                            if(i!=path.size()-1){System.out.print(", ");}
+                        }
+                        System.out.print("\n\n");
                     }
-                    System.out.print("\n\n\n");
-                }
-                else{
-                    System.out.print("\nNot OK\n\n");
-                }
-            }
-            if("-p".equals(ch))
-            {// ALPHA BETA PRUNING
-                if(mainMinMax.tree!=null){
-                    mainMinMax.tree.minMax();
-                    System.out.print("\n["+mainMinMax.tree.size()+","+(int)mainMinMax.tree.prunedNodes()+']'+" ");
-                    double rt=mainMinMax.tree.minMax();
-                    ArrayList<Integer> path = mainMinMax.tree.optimalPath();
-                    for(int i=0; i<path.size(); i++){
-                        System.out.print(path.get(i)+"");
-                        if(i!=path.size()-1){System.out.print(", ");}
+                    else{
+                        System.out.print("\nNot OK\n\n");
                     }
-                    System.out.print("\n\n");
-                }  
-                else{
-                    System.out.print("\nNot OK\n\n");
                 }
-            }
-            if("-q".equals(ch))
-            {
-                sc.close();
-                System.out.print("\n");
-                return;
-            }
+                else if("-p".equals(ch))
+                {// ALPHA BETA PRUNING
+                    if(mainMinMax.tree!=null){
+                        mainMinMax.tree.minMax();
+                        System.out.print("\n["+mainMinMax.tree.size()+","+(int)mainMinMax.tree.prunedNodes()+']'+" ");
+                        ArrayList<Integer> path = mainMinMax.tree.optimalPath();
+                        for(int i=0; i<path.size(); i++){
+                            System.out.print(path.get(i));
+                            if(i!=path.size()-1){
+                                System.out.print(", ");
+                            }
+                        }
+                        System.out.print("\n\n");
+                    }  
+                    else{
+                        System.out.print("\nNot OK\n\n");
+                    }
+                }
+                else if("-q".equals(ch))
+                {
+                    sc.close();
+                    //System.out.print("\n");
+                    return;
+                }
             }catch(NoSuchElementException ex) {
-                sc.next();
+                //System.out.print("\n");
+                //sc.next();
+                //continue;
             }
         }
     }
-    
-    public boolean isJson(String strBuilder){
+}
+/*    public boolean isJson(String strBuilder){
         this.Jstr = strBuilder;
         try {
             JSONObject myJsonObject=new JSONObject(strBuilder);
         } catch (JSONException | IllegalArgumentException e) {
-            System.out.println("\nInvalid format\n\n");
+            System.out.print("\nInvalid format\n\n");
             return false;
         }
         return true;
     }
     
 }
+*/
